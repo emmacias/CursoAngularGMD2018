378cc15bd3bb9c409ec5d43836f836f5dc186b6a
Jose Diaz <jose.diaz@joedayz.pe>
2018-03-12 21:26:05 -0500

    dynamic components

---
+++ profesional/dynamic-components/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component, AfterContentInit, ViewChild, ViewContainerRef, ComponentFactoryResolver } from '@angular/core';
+
+import { User } from './auth-form/auth-form.interface';
+import { AuthFormComponent } from './auth-form/auth-form.component';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <div>
+      <div #entry></div>
+    </div>
+  `
+})
+export class AppComponent implements AfterContentInit {
+
+  @ViewChild('entry', { read: ViewContainerRef}) entry: ViewContainerRef;
+
+  constructor(private resolver: ComponentFactoryResolver){}
+
+
+  ngAfterContentInit(): void {
+    const authFormFactory = this.resolver.resolveComponentFactory(AuthFormComponent);
+    const component = this.entry.createComponent(authFormFactory);
+
+  }
+  loginUser(user: User) {
+    console.log('Login', user);
+  }
+
+}
