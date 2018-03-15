3cdddc9724551f9ef13ff74325ffb383a29f6557
Jose Diaz <jose.diaz@joedayz.pe>
2018-03-14 20:04:28 -0500

    change strategy

--- profesional/viewencapsulation/src/app/app.component.ts
+++ profesional/viewencapsulation/src/app/app.component.ts
@@ -4,11 +4,43 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <div>
-      <example-one></example-one>
-      <example-two></example-two>
-      <example-three></example-three>
+    
+      <button (click)="addProp"> Add property </button>
+      <button (click)="changeUser"> Change user object </button>
+      <button (click)="changeName"> Change name property <button>
+
+      <div class="users">
+        <example-one [user]="user"></example-one>
+
+        <example-two [user]="user"></example-two>
+      </div>
+
     </div>
   `
 })
 export class AppComponent {
+
+  user: any = {
+    name: "Juan Perez",
+    age: 44,
+    location: "Callao"
+  };
+
+  addProp(){
+    this.user.email = 'javierperez@gmail.com';
+  }
+
+  changeName(){
+    this.user.name = 'John Perez';
+  }
+
+  changeUser(){
+    this.user = {
+      name: "Tom Perez",
+      age: 41,
+      location: "Lima"
+    };
+  }
+
+
 }
