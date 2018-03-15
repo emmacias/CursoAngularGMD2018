5942a43a57da3ec5bee3fb7d2d13dbfb665a9369
Jose Diaz <jose.diaz@joedayz.pe>
2018-03-14 20:21:39 -0500

    creando directiva

--- profesional/custom-attribute-directive/src/app/app.component.ts
+++ profesional/custom-attribute-directive/src/app/app.component.ts
@@ -2,8 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div>
+    <label>
+      Credit Card Number
+      <input 
+        name="credit-card" 
+        type="text"
+        placeholder="Enter your 16-digit card number"
+        credit-card>
+    </label>
+    </div>
+  
+  `
 })
 export class AppComponent {
   title = 'app';
