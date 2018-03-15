ad6c655c56dce751e0749ca24d5bc0513366bcd5
Jose Diaz <jose.diaz@joedayz.pe>
2018-03-14 20:37:17 -0500

    directive tooltip

--- profesional/custom-attribute-directive/src/app/credit-card/credit-card.directive.ts
+++ profesional/custom-attribute-directive/src/app/credit-card/credit-card.directive.ts
@@ -3,9 +3,7 @@ import { Directive, ElementRef, HostListener, HostBinding } from '@angular/core'
 
 @Directive({ selector: '[credit-card]' })
 export class CreditCardDirective {
-    constructor(private element: ElementRef) { 
-        console.log(this.element);
-    }
+
 
     @HostBinding('style.border')
     border: string;
