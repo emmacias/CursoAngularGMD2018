d33ac576f2f3b74a9adb9fb1106901237c9349d2
Jose Diaz <jose.diaz@joedayz.pe>
2018-03-19 19:13:53 -0500

    add stock

--- profesional/forms/src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
+++ profesional/forms/src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
@@ -9,11 +9,19 @@ import { Product } from '../../models/product.interface';
     <div class="stock-inventory">
         <form [formGroup]="form" (ngSubmit)="onSubmit()">
 
-          <stock-branch [parent]="form"></stock-branch>
+          <stock-branch 
+            [parent]="form">
+          </stock-branch>
 
-          <stock-selector [parent]="form" [products]="products"></stock-selector>
+          <stock-selector 
+              [parent]="form" 
+              [products]="products"
+              (added)="addStock($event)">
+          </stock-selector>
 
-          <stock-products [parent]="form"></stock-products>
+          <stock-products 
+            [parent]="form">
+          </stock-products>
 
  
           <div class="stock-inventory_buttons">
@@ -60,6 +68,12 @@ export class StockInventoryComponent {
           product_id: new FormControl(parseInt(stock.product_id, 10) || ''),
           quantity: new FormControl(stock.quantity || 10) 
       });
+    }
+
+
+    addStock(stock){
+      const control = this.form.get('stock') as FormArray;
+      control.push(this.createStock(stock));
     }
 
     onSubmit(){
